df08203a38e1c254918aae061a22ccf252da8e11
Fiol Thiago <fiolthiago9@gmail.com>
2024-01-22 18:29:55 -0300

    añadimos estilos con boostrap y css a la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Container } from "react-bootstrap"
 import FormularioTarea from "./components/FormularioTarea";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
     <Container className="my-4 mainPage">
-      <h1 className="text-center">Lista de tareas</h1>
+      <h1 className="text-center text-light">Lista de tareas</h1>
       <FormularioTarea></FormularioTarea>
     </Container>)
 }
