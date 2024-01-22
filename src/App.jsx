00e941ea103825d8f1bba07927bdfdd701a3e578
Fiol Thiago <fiolthiago9@gmail.com>
2024-01-22 18:05:21 -0300

    añadimos el boostrap y tambien añadimos el componente formulario tarea

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Container } from "react-bootstrap"
+import FormularioTarea from "./components/FormularioTarea";
+
 function App() {
-  return 
+  return (
+    <Container className="my-4 mainPage">
+      <h1 className="text-center">Lista de tareas</h1>
+      <FormularioTarea></FormularioTarea>
+    </Container>)
 }
 
 export default App
